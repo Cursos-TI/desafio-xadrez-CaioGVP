1a710fccd08f63ed3a2ff57c3172d47fe62b3083
CaioGVP <90455334+CaioGVP@users.noreply.github.com>
2026-03-18 13:34:43 +0000

    Nível Novato - Movimentação das Peças

--- xadrez.c
+++ xadrez.c
@@ -8,14 +8,196 @@ int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    char peca;
+    int t = 1, b = 1, r = 1;
+
+        printf("Escolha a peca: \n");
+    printf("T. Torre\n");
+    printf("B. Bispo\n");
+    printf("R. Rainha\n");
+    scanf(" %c", &peca);
+
+    printf("\n");
+
+
+    switch (peca)
+    {
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
 
+    case 'T':
+    case 't':
+        
+        printf("Voce escolheu a Torre\n\n");
+
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("8 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("7 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("6 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("5 |   |   | T |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("4 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("3 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("2 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("1 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("    a   b   c   d   e   f   g   h\n\n");
+
+        while (t <= 5)
+        {
+            printf("Direita\n\n");
+
+            t++;
+        }
+
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("8 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("7 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("6 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("5 |   |   |   |   |   |   |   | T |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("4 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("3 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("2 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("1 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("    a   b   c   d   e   f   g   h\n\n");
+    
+        break;
+
+    // Implementação de Movimentação do Bispo
+    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+
+    case 'B':
+    case 'b':
+
+        
+        printf("Voce escolheu a Bispo\n\n");
+
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("8 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("7 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("6 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("5 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("4 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("3 |   | B |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("2 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("1 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("    a   b   c   d   e   f   g   h\n\n");
+
+        do
+        {
+            printf("Diagonal para cima e a direita\n\n");
+            b++;
+
+        } while (b <= 5);              
+        
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("8 |   |   |   |   |   |   | B |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("7 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("6 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("5 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("4 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("3 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("2 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("1 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("    a   b   c   d   e   f   g   h\n\n");
+    
+        break;
+
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+
+    case 'R':
+    case 'r':
+        
+        printf("Voce escolheu a Rainha\n\n");
+
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("8 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("7 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("6 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("5 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("4 |   |   |   |   |   |   |   | R |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("3 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("2 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("1 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("    a   b   c   d   e   f   g   h\n\n");
+        
+        for (;r <= 8; r++)
+        {
+            printf("Esquerda\n\n");
+            
+        }
+
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("8 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("7 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("6 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("5 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("4 | R |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("3 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("2 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("1 |   |   |   |   |   |   |   |   |\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("    a   b   c   d   e   f   g   h\n\n");        
+
+        break;
+    
+    default:
+        printf("Opcao invalida!\n");
+        break;
+    }
+
+    
+
+
+    return 0;
+}  
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
